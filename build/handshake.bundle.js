695e97aaa2ee79c22b4e16b8da61ae87327cc48d
Julian <jutanke@googlemail.com>
2014-12-16 17:04:38 +0100

    optimized for npm / browserify

---
+++ build/handshake.bundle.js
@@ -0,0 +1,201 @@
+!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.Handshake=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/**
+ * Created by Julian on 12/11/2014.
+ */
+// run adapterjs
+require("webrtc-adapter");
+
+var Utils = require("yutils");
+
+function test() {
+    console.log("TEST");
+}
+
+exports.Handshake = {
+
+    test: test
+
+};
+},{"webrtc-adapter":2,"yutils":3}],2:[function(require,module,exports){
+/*jslint node:true*/
+/*globals RTCPeerConnection, mozRTCPeerConnection, webkitRTCPeerConnection */
+/*globals RTCSessionDescription, mozRTCSessionDescription */
+/*globals RTCIceCandidate, mozRTCIceCandidate */
+'use strict';
+
+var myRTCPeerConnection = null;
+var myRTCSessionDescription = null;
+var myRTCIceCandidate = null;
+
+var renameIceURLs = function (config) {
+  if (!config) {
+    return;
+  }
+  if (!config.iceServers) {
+    return config;
+  }
+  config.iceServers.forEach(function (server) {
+    server.url = server.urls;
+    delete server.urls;
+  });
+  return config;
+};
+
+var fixChromeStatsResponse = function(response) {
+  var standardReport = {};
+  var reports = response.result();
+  reports.forEach(function(report) {
+    var standardStats = {
+      id: report.id,
+      timestamp: report.timestamp,
+      type: report.type
+    };
+    report.names().forEach(function(name) {
+      standardStats[name] = report.stat(name);
+    });
+    standardReport[standardStats.id] = standardStats;
+  });
+
+  return standardReport;
+};
+
+var sessionHasData = function(desc) {
+  if (!desc) {
+    return false;
+  }
+  var hasData = false;
+  var prefix = 'm=application';
+  desc.sdp.split('\n').forEach(function(line) {
+    if (line.slice(0, prefix.length) === prefix) {
+      hasData = true;
+    }
+  });
+  return hasData;
+};
+
+// Unify PeerConnection Object.
+if (typeof RTCPeerConnection !== 'undefined') {
+  myRTCPeerConnection = RTCPeerConnection;
+} else if (typeof mozRTCPeerConnection !== 'undefined') {
+  myRTCPeerConnection = function (configuration, constraints) {
+    // Firefox uses 'url' rather than 'urls' for RTCIceServer.urls
+    var pc = new mozRTCPeerConnection(renameIceURLs(configuration), constraints);
+
+    // Firefox doesn't fire 'onnegotiationneeded' when a data channel is created
+    // https://bugzilla.mozilla.org/show_bug.cgi?id=840728
+    var dataEnabled = false;
+    var boundCreateDataChannel = pc.createDataChannel.bind(pc);
+    pc.createDataChannel = function(label, dataChannelDict) {
+      var dc = boundCreateDataChannel(label, dataChannelDict);
+      if (!dataEnabled) {
+        dataEnabled = true;
+        if (pc.onnegotiationneeded &&
+            !sessionHasData(pc.localDescription) &&
+            !sessionHasData(pc.remoteDescription)) {
+          var event = new Event('negotiationneeded');
+          pc.onnegotiationneeded(event);
+        }
+      }
+      return dc;
+    };
+
+    return pc;
+  };
+} else if (typeof webkitRTCPeerConnection !== 'undefined') {
+  // Chrome returns a nonstandard, non-JSON-ifiable response from getStats.
+  myRTCPeerConnection = function(configuration, constraints) {
+    var pc = new webkitRTCPeerConnection(configuration, constraints);
+    var boundGetStats = pc.getStats.bind(pc);
+    pc.getStats = function(selector, successCallback, failureCallback) {
+      var successCallbackWrapper = function(chromeStatsResponse) {
+        successCallback(fixChromeStatsResponse(chromeStatsResponse));
+      };
+      // Chrome also takes its arguments in the wrong order.
+      boundGetStats(successCallbackWrapper, failureCallback, selector);
+    };
+    return pc;
+  };
+}
+
+// Unify SessionDescrption Object.
+if (typeof RTCSessionDescription !== 'undefined') {
+  myRTCSessionDescription = RTCSessionDescription;
+} else if (typeof mozRTCSessionDescription !== 'undefined') {
+  myRTCSessionDescription = mozRTCSessionDescription;
+}
+
+// Unify IceCandidate Object.
+if (typeof RTCIceCandidate !== 'undefined') {
+  myRTCIceCandidate = RTCIceCandidate;
+} else if (typeof mozRTCIceCandidate !== 'undefined') {
+  myRTCIceCandidate = mozRTCIceCandidate;
+}
+
+exports.RTCPeerConnection = myRTCPeerConnection;
+exports.RTCSessionDescription = myRTCSessionDescription;
+exports.RTCIceCandidate = myRTCIceCandidate;
+
+},{}],3:[function(require,module,exports){
+/**
+ * Created by Julian on 12/10/2014.
+ */
+(function (exports) {
+
+    // performance.now polyfill
+    var perf = null;
+    if (typeof performance === 'undefined') {
+        perf = {};
+    } else {
+        perf = performance;
+    }
+
+    perf.now = perf.now || perf.mozNow || perf.msNow ||  perf.oNow || perf.webkitNow || Date.now ||
+        function () {
+            return new Date().getTime();
+        };
+
+    function swap(array, i, j) {
+        if (i !== j) {
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+
+    /*
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     */
+
+    var getRandomInt = exports.getRandomInt = function (min, max) {
+        if (min > max) throw new Error("min must be smaller than max! {" + min + ">" + max + "}" );
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+
+    exports.sample = function (list, n) {
+        var result = [], j,i = 0, L = n > list.length ? list.length : n, s = list.length - 1;
+        for(;i<L;i++) {
+            j = getRandomInt(i,s);
+            swap(list,i,j);
+            result.push(list[i]);
+        }
+        return result;
+    };
+
+    exports.assertLength = function (arg, nbr) {
+        if (arg.length === nbr) return true;
+        else throw new Error("Wrong number of arguments: expected:" + nbr + ", but got: " + arg.length);
+    };
+
+    exports.guid = function () {
+        var d = perf.now();
+        var guid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = (d + Math.random() * 16) % 16 | 0;
+            d = Math.floor(d / 16);
+            return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+        });
+        return guid;
+    };
+
+})(typeof exports === 'undefined' ? this['yUtils'] = {} : exports);
+},{}]},{},[1])(1)
+});
